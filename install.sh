bf8bd372128dcbaf1bab2390a42f0edb647ba6ea
-k <slowdive@me.com>
2019-08-29 01:01:44 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
-###################
-# Install ansible #
-if ! grep -q "ansible/ansible" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-    echo "Adding Ansible PPA"
-    sudo apt-add-repository ppa:ansible/ansible -y
-fi
+echo "Adding Ansible PPA"
+sudo apt update
+sudo apt install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible -y
 
-if ! hash ansible >/dev/null 2>&1; then
-    echo "Installing Ansible..."
-    sudo apt-get update
-    sudo apt-get install software-properties-common ansible git python-apt -y
-else
-    echo "Ansible already installed"
-fi
 
-#####################################
-# Display real installation process #
-echo ""
+echo "Installing Ansible..."
+sudo apt-get update
+sudo apt-get install ansible -y
 echo "DONE"
-echo ""
