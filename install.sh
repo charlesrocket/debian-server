9fc20ea536d9ec015cb8887580447638fb56ae30
-k <slowdive@me.com>
2019-08-29 02:18:18 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-echo "Adding Ansible PPA"
-sudo apt update
-sudo apt install software-properties-common -y
-sudo apt-add-repository --yes ppa:ansible/ansible
-
 echo "Installing Ansible..."
 sudo apt update
 sudo apt install ansible -y
