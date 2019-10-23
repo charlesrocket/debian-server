b9f85bec6022c625efb9f55a2d972173db2e83ed
-k <slowdive@me.com>
2019-10-23 16:24:29 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ cat << "EOF"
  ░ ▒  ▒  ▒ ░░ ░▒  ░ ░  ░  ▒     ░ ▒ ▒░
  ░ ░  ░  ▒ ░░  ░  ░  ░        ░ ░ ░ ▒
    ░     ░        ░  ░ ░          ░ ░
- ░                   ░                 
+ ░                   ░
 
 EOF
 echo "Installing Ansible..."
